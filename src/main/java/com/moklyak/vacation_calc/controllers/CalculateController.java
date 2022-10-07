1aa4c581db0b113143279cacd42612d8785cfffb
MSI <nikita.dezertir@gmail.com>
2022-10-08 02:27:31 +0400

    remove some garbage of main and additional task implemented

--- src/main/java/com/moklyak/vacation_calc/controllers/CalculateController.java
+++ src/main/java/com/moklyak/vacation_calc/controllers/CalculateController.java
@@ -10,28 +10,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DateFormat;
-import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/calculate")
 public class CalculateController {
-    private final DecimalFormat df = new DecimalFormat(".##");
-    private final DateFormat holidayDateFormat;
     private final static DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
     private final List<Date> holidays;
     private final Calendar calendar = Calendar.getInstance();
 
     @Autowired
-    public CalculateController(@Qualifier("holidays") DateFormat holidayDateFormat,
-                               @Qualifier("holidays") List<Date> holidays) {
-        this.holidayDateFormat = holidayDateFormat;
+    public CalculateController(@Qualifier("holidays") List<Date> holidays) {
         this.holidays = holidays;
     }
 
@@ -41,9 +35,9 @@ public class CalculateController {
      * Returns the vacation payment for specified average month salary and count of vacations days or
      * if specified dates of actual vacation dates returns the vacation payment without including
      * holidays and weekends (why without weekends idk but saying me that I should take into account the given condition
-     * but the law says that it should count as workdays so i should'nt) <br>
+     * but the law says that it should count as workdays, so I shouldn't) <br>
      * if actual vacation days specified the @param vacDaysCount ignored :(
-     * @param avgMonthSalary - average salary for month
+     * @param avgMonthSalary - average salary for month in kopecks
      * @param vacDaysCount - count of vacation days
      * @param vacDays - string array of actual vacation days, date (day) must be in format dd.MM.yyyy
      *                where dd is day of the month, MM is month of the year and yyyy is year ща Anno Domini
@@ -65,22 +59,20 @@ public class CalculateController {
                                 }
                             }
                     )
-                    .filter(x -> {
-                        return holidays.stream().noneMatch(y -> {
-                            int holDay, holMon, vacDay, vacMon, vacDayOfWeek;
-                            calendar.setTime(y);
-                            holDay = calendar.get(Calendar.MONTH);
-                            holMon = calendar.get(Calendar.DAY_OF_MONTH);
-                            calendar.setTime(x);
-                            vacDay = calendar.get(Calendar.MONTH);
-                            vacMon = calendar.get(Calendar.DAY_OF_MONTH);
-                            vacDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-                            return holDay != vacDay &&
-                                    holMon != vacMon ||
-                                    vacDayOfWeek != Calendar.SATURDAY ||
-                                    vacDayOfWeek != Calendar.SUNDAY;
-                        });
-                    }).count();
+                    .filter(x -> holidays.stream().noneMatch(y -> {
+                        int holDay, holMon, vacDay, vacMon, vacDayOfWeek;
+                        calendar.setTime(y);
+                        holDay = calendar.get(Calendar.MONTH);
+                        holMon = calendar.get(Calendar.DAY_OF_MONTH);
+                        calendar.setTime(x);
+                        vacDay = calendar.get(Calendar.MONTH);
+                        vacMon = calendar.get(Calendar.DAY_OF_MONTH);
+                        vacDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+                        return  holDay != vacDay &&
+                                holMon != vacMon ||
+                                vacDayOfWeek != Calendar.SATURDAY ||
+                                vacDayOfWeek != Calendar.SUNDAY;
+                    })).count();
             return Math.round(avgMonthSalary / AVG_DAY_IN_MONTH_COUNT * actualCount);
         }
     }
